a64ba700ea8fc94291cc2abd73b129e1ebfcb4c2
Dymedrol <dmitrykob@gmail.com>
2020-07-05 17:04:55 +0700

    Личный проект: начинаем программировать

---
+++ src/components/svg/svg.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const PlayS = () => {
+  return <svg width="19" height="19" viewBox="0 0 19 19" fill="none">
+    <path fillRule="evenodd" clipRule="evenodd" d="M0 0L19 9.5L0 19V0Z" fill="#EEE5B5"/>
+  </svg>;
+};
+
+export const Add = () => {
+  return <svg width="19px" height="20px" viewBox="0 0 19 20" version="1.1">
+    <title>+</title>
+    <desc>Created with Sketch.</desc>
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <polygon id="+" fill="#EEE5B5" points="10.777832 11.2880859 10.777832 19.5527344 8.41650391 19.5527344 8.41650391 11.2880859 0.627929688 11.2880859 0.627929688 8.92675781 8.41650391 8.92675781 8.41650391 0.662109375 10.777832 0.662109375 10.777832 8.92675781 18.5664062 8.92675781 18.5664062 11.2880859"></polygon>
+    </g>
+  </svg>;
+};
+
