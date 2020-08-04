64e514da4433a3e9ef0f550e2e7c67b8e2a2d7f0
Dymedrol <dmitrykob@gmail.com>
2020-08-04 16:53:57 +0700

    починил ссылку в хедере в sign-in

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import {Redirect} from "react-router-dom";
 import PropTypes from "prop-types";
+import {Link} from 'react-router-dom';
 
 import {Errors, LoginStatus, AppRoute} from '../../const.js';
 
@@ -56,13 +57,17 @@ class SignIn extends PureComponent {
 
     return (
       <div className="user-page">
+
         <header className="page-header user-page__head">
           <div className="logo">
-            <a href="main.html" className="logo__link">
+            <Link
+              className="logo__link"
+              to={AppRoute.ROOT}
+            >
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
 
           <h1 className="page-title user-page__title">Sign in</h1>
